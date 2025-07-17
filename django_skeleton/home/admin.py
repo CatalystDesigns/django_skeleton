81e0d8306949eb5508f298f4cf19c9bd77954cf6
Nancy Robinson <catalystdesigns.net@gmail.com>
2025-07-17 15:26:02 -0600

    adding skeleton files that are needed to show the home page

--- django_skeleton/home/admin.py
+++ django_skeleton/home/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
+# class HomeAdmin(admin.ModelAdmin):
+#   list_display = ('name', 'spec1', 'spec2')
+# admin.site.register(Home, HomeAdmin)
--- django_skeleton/home/models.py
+++ django_skeleton/home/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Home(models.Model):
+    pass
+
+
+# in this class, list model characteristics
+'''name = models.CharField(max_length=64)
+    price = models.FloatField()
+    stock = models.IntegerField()
+    image_url = models.CharField('2083')'''
